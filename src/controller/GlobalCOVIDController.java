3196c29ca88bc9e5c391068bc66ba89f1d8fdc7b
Nchathura <52320494+nchathura@users.noreply.github.com>
2020-05-12 21:46:41 +0530

    update

--- src/controller/GlobalCOVIDController.java
+++ src/controller/GlobalCOVIDController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class GlobalCOVIDController {
+    public AnchorPane root;
+
+    public void btnHome_OnAction(ActionEvent actionEvent) throws IOException {
+       Scene dashStage = new Scene(FXMLLoader.load(getClass().getResource("/view/Dashboard.fxml")));
+       Stage primaryStage = (Stage) (root.getScene().getWindow());
+       primaryStage.setScene(dashStage);
+       primaryStage.centerOnScreen();
+       primaryStage.show();
+    }
 }
--- src/controller/ManageQuarantineCentersController.java
+++ src/controller/ManageQuarantineCentersController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ManageQuarantineCentersController {
+    public AnchorPane root;
+
+    public void btnHome_OnAction(ActionEvent actionEvent) throws IOException {
+        Scene dashScene = new Scene(FXMLLoader.load(getClass().getResource("/view/Dashboard.fxml")));
+        Stage primaryStage = (Stage) (root.getScene().getWindow());
+        primaryStage.setScene(dashScene);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
 }
